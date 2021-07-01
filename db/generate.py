1ad9f1ceb10b7f8dc70eb80effb58c3cf750db87
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2021-07-01 16:16:57 +0200

    Re-generate DB

--- db/generate.py
+++ db/generate.py
@@ -15,7 +15,7 @@ def boolrand(): return rand(0, 2) == 0
 def rand_datetime(start, end):
     return start + random.random() * (end - start)
 
-def rand_datetime_def(): return rand_datetime(dt_fromstr("2021-01-01 0:0:0", ALLFMT), dt_fromstr("2021-06-21 23:59:59", ALLFMT))
+def rand_datetime_def(): return rand_datetime(dt_fromstr("2020-08-01 0:0:0", ALLFMT), dt_fromstr("2021-07-01 23:59:59", ALLFMT))
 
 def time_add_days(dt, ds):
     return dt + datetime.timedelta(days = ds)
@@ -38,12 +38,12 @@ def dt_fromstr(datestr, fmt): return datetime.datetime.fromtimestamp(time.mktime
 NUM_USR = 11
 NUM_GAMES = 14
 
-piano_query = "insert into Piano(id, id_usr, tipologia, data_acquisto, ora_acquisto) values"
+piano_query = "insert into Piano(id, id_usr, tipologia, data_acquisto, ora_acquisto, data_fine, ora_fine) values"
 cv_query = "insert into CopiaVideogioco(id, id_vg, id_usr, data_acquisto) values"
 part_query = "insert into Partita values"
 pref_query = "insert into Preferenza values"
 canc_query = "insert into PianoCancellato values"
-sess_query = "insert into Sessione(id, id_vg, id_creatore, data, tempo_trascorso) values"
+sess_query = "insert into Sessione(id, id_vg, id_creatore, data, ora, tempo_trascorso) values"
 partecip_query = "insert into Partecipazione(id_usr, id_session) values"
 copy_query = "insert into CopiaVideogioco values"
 acquisto_query = "insert into Acquisto values"
@@ -51,20 +51,33 @@ id_piano = 1
 
 def add_piano(name, date):
     global id_piano
+
+    if date > dt_fromstr("2021-07-01 23:59:59", ALLFMT):
+        return date
+
+    # sometimes we don't add new plans
     if name != "Gratuito" and not boolrand():
         return date
-    print("{}({}, {}, '{}', '{}', '{}');".format(piano_query, id_piano, u, name, dt_date(date), dt_hour(rand_datetime_def())))
+
+    end_date = None
+    if name == "Gratuito": end_date = time_add_month(date)
+    elif name == "Mensile": end_date = time_add_month(date)
+    elif name == "Annuale": end_date = time_add_year(date)
+
+    print("{}({}, {}, '{}', '{}', '{}', '{}', '{}');".format(
+        piano_query, id_piano, u, name, dt_date(date), dt_hour(rand_datetime_def()), dt_date(end_date), dt_hour(rand_datetime_def())
+        ))
     id_piano += 1
     if name == "Gratuito":
-        return time_add_month(date)
+        return end_date
+    # random cancel plan
     if boolrand():
-        date = time_add_days(date, rand(1, 28))
-        print("{}({}, '{}', '{}');".format(canc_query, id_piano-1, dt_date(date), dt_hour(rand_datetime_def())))
-        return date
-    elif name == "Annuale":
-        return time_add_year(date)
-    else:
-        return time_add_month(date)
+        cancdate = time_add_days(date, rand(1, 28))
+        if cancdate > dt_fromstr("2021-07-01 23:59:59", ALLFMT):
+            cancdate = dt_fromstr("2021-07-01 23:59:59", ALLFMT)
+        print("{}({}, '{}', '{}');".format(canc_query, id_piano-1, dt_date(cancdate), dt_hour(rand_datetime_def())))
+        return cancdate
+    return end_date
 
 multiplayers = [1, 5, 6, 8, 11, 14]
 mult_nums = {
@@ -101,7 +114,8 @@ for u in range(1, NUM_USR+1):
             continue # user has never played this game and won't give any judgement
         # generate some matches
         for i in range(0, rand(1, 11)):
-            print("{}({}, {}, '{}', {});".format(part_query, u, vg, dt_str(rand_datetime(start_date, end_date)), rand(1, 5)))
+            tmp = rand_datetime(start_date, end_date)
+            print("{}({}, {}, '{}', '{}', {});".format(part_query, u, vg, dt_date(tmp), dt_hour(tmp), rand(1, 5)))
         # add preference
         if boolrand():
             print("{}({}, {});".format(pref_query, u, vg))
@@ -115,7 +129,8 @@ for u in range(1, NUM_USR+1):
                 id_copy += 1
             if id_copy < len(vg_copies):
                 vg_copies[id_copy][2] = True
-                print("{}({}, {}, '{}');".format(acquisto_query, vg_copies[id_copy][0], u, dt_str(rand_datetime(start_date, end_date))))
+                tmpdt = rand_datetime(start_date, end_date)
+                print("{}({}, {}, '{}', '{}');".format(acquisto_query, vg_copies[id_copy][0], u, dt_date(tmpdt), dt_hour(tmpdt)))
         if vg not in multiplayers:
             continue
         # search for sessions for this game
@@ -129,6 +144,7 @@ for s in sessions:
     if l < 2 or l < num_players[0] or l > num_players[1]:
         continue
     creator = s[4][rand(0, len(s[4]))]
-    print("{}({}, {}, {}, '{}', {});".format(sess_query, s[0], s[1], creator, dt_date(s[2]), s[3]))
+    print("{}({}, {}, {}, '{}', '{}', {});".format(sess_query, s[0], s[1], creator, dt_date(s[2]), dt_hour(s[2]), s[3]))
     for usr in s[4]:
         print("{}({}, {});".format(partecip_query, usr, s[0]))
+
---
+++ db/template.sql
@@ -0,0 +1,169 @@
+create database if not exists OnlineConsole;
+use OnlineConsole;
+
+drop table if exists Acquisto;
+drop table if exists CopiaVideogioco;
+drop table if exists Partita;
+drop table if exists Preferenza;
+drop table if exists PianoCancellato;
+drop table if exists Piano;
+drop table if exists TipologiaPiano;
+drop table if exists Partecipazione;
+drop table if exists Sessione;
+drop table if exists VideogiocoMultiplayer;
+drop table if exists Utente;
+drop table if exists Videogioco;
+
+create table Utente (
+    id              int                 primary key auto_increment,
+    nome            varchar(255)        not null,
+    cognome         varchar(255)        not null,
+    password        varchar(255)        not null,
+    email           varchar(255)        not null,
+    telefono        int
+);
+
+create table Videogioco (
+    id              int                 primary key auto_increment,
+    titolo          varchar(255)        not null,
+    genere          varchar(255),
+    anno            year,
+    azienda         varchar(255),
+    produttore      varchar(255),
+    prezzo          int
+);
+
+create table CopiaVideogioco (
+    id              int                 primary key auto_increment,
+    id_vg           int                 not null references Videogioco(id)
+);
+
+create table Acquisto (
+    id_copia        int                 not null references CopiaVideogioco(id),
+    id_usr          int                 not null references Utente(id),
+    data_acquisto   date,
+    ora_acquisto    time,
+    unique (id_copia, id_usr)
+);
+
+create table Partita (
+    id_usr          int                 not null references Utente(id),
+    id_vg           int                 not null references Videogioco(id),
+    data            date                not null,
+    ora             time                not null,
+    ore_gioco       int,
+    unique (id_usr, id_vg, data, ora)
+);
+
+create table Preferenza (
+    id_usr          int                 not null references Utente(id),
+    id_vg           int                 not null references Videogioco(id),
+    unique (id_usr, id_vg)
+);
+
+create table VideogiocoMultiplayer (
+    id_vg           int                 primary key references Videogioco(id),
+    min_giocatori   int                 default 1,
+    max_giocatori   int
+);
+
+create table TipologiaPiano (
+    tipologia       enum('Gratuito',
+                         'Mensile',
+                         'Annuale')     primary key,
+    costo           int                 not null
+);
+
+create table Piano (
+    id              int                 primary key auto_increment,
+    id_usr          int                 not null references Utente(id),
+    tipologia       enum('Gratuito',
+                         'Mensile',
+                         'Annuale')     not null references TipologiaPiano(tipologia),
+    data_acquisto   date                not null,
+    ora_acquisto    time                not null,
+    data_fine       date                not null,
+    ora_fine        time                not null
+);
+
+create table PianoCancellato (
+    id              int                 primary key references Piano(id),
+    data_canc       date                not null,
+    ora_canc        time                not null
+);
+
+create table Sessione (
+    id              int                 primary key,
+    id_vg           int                 not null references VideogiocoMultiplayer(id_vg),
+    id_creatore     int                 not null references Utente(id),
+    data            date,
+    ora             time,
+    tempo_trascorso int
+);
+
+create table Partecipazione (
+    id_usr          int                 not null references Utente(id),
+    id_session      int                 not null references Sessione(id),
+    unique(id_usr, id_session)
+);
+
+create index ind_utente_id                  on Utente(id);
+create index ind_vg_id                      on Videogioco(id);
+create index ind_cv_id                      on CopiaVideogioco(id);
+create index ind_vgm_id_vg                  on VideogiocoMultiplayer(id_vg);
+create index ind_tipo_tipo                  on TipologiaPiano(tipologia);
+create index ind_piano_id                   on Piano(id);
+create index ind_piano_canc_id              on PianoCancellato(id);
+
+create index indFK_cv_id_vg                 on CopiaVideogioco(id_vg);
+create index indFK_acq_id_copia             on Acquisto(id_copia);
+create index indFK_acq_id_usr               on Acquisto(id_usr);
+create index indFK_partita_id_usr           on Partita(id_usr);
+create index indFK_partita_id_vg            on Partita(id_vg);
+create index indFK_preferenza_id_usr        on Preferenza(id_usr);
+create index indFK_preferenza_id_vg         on Preferenza(id_vg);
+create index indFK_piano_id_usr             on Piano(id_usr);
+create index indFK_piano_tipologia          on Piano(tipologia);
+create index indFK_sessione_id_vg           on Sessione(id_vg);
+create index indFK_sessione_id_creatore     on Sessione(id_creatore);
+create index indFK_part_id_usr              on Partecipazione(id_usr);
+create index indFK_part_id_session          on Partecipazione(id_session);
+
+insert into Utente(nome, cognome, password, email) values('Christian', 'Ricci', 'abc', 'chri@mail.com');
+insert into Utente(nome, cognome, password, email) values('Roberto', 'Montalti', 'def', 'rob@mail.com');
+insert into Utente(nome, cognome, password, email) values('Juri', 'Simoncini', 'ghi', 'juri@mail.com');
+insert into Utente(nome, cognome, password, email) values('Edoardo', 'Savini', 'jkl', 'sedoardo98@mail.com');
+insert into Utente(nome, cognome, password, email) values('Giacomo', 'Cavalieri', 'mno', 'giaccava@mail.com');
+insert into Utente(nome, cognome, password, email) values('Chad', 'Smith', 'pqr', 'chad@mail.com');
+insert into Utente(nome, cognome, password, email) values('Federico', 'Orazio', 'stu', 'orazio@mail.com');
+insert into Utente(nome, cognome, password, email) values('Dario', 'Ketchup', 'vwx', 'ketchup@mail.com');
+insert into Utente(nome, cognome, password, email) values('Gianluca', 'Migliarini', 'yz', 'gianlu@mail.com');
+insert into Utente(nome, cognome, password, email) values('Simone', 'Rega', 'abcdef', 'rega@mail.com');
+insert into Utente(nome, cognome, password, email) values('Francesco', 'Tappi', 'ghijk', 'tappi@mail.com');
+
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Super Mario World', 'Platformer', 1990, 'Nintendo', 'Shigeru Miyamoto', 30);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Touhou 6', 'Danmaku', 2002, 'Team Alice', 'ZUN', 20);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('DOOM', 'FPS', 1993, 'id Software', 'John Carmack', 20);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('EarthBound', 'JRPG', 1995, 'Nintendo', 'Shigesato Itoi', 30);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Overwatch', 'FPS', 2016, 'Blizzard', 'Jeff Kaplam', 60);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Genshin Impact', 'ARPG', 2020, 'miHoYo', NULL, 20);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Celeste', 'Platformer', 2018, 'Extremely OK Games', 'Maddy Thorson', 20);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Rocket League', 'Sport', 2015, 'Psyonix Studios', NULL, 30);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('DOOM Eternal', 'FPS', 2020, 'id Software', 'Hugo Martin', 60);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Super Mario 64', 'Platformer', 1997, 'Nintendo', 'Shigeru Myamoto', 20);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Rainbow Six Siege', 'FPS', 2015, 'Ubisoft', 'Xavier Marquis', 60);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Final Fantasy VII', 'JRPG', 1997, 'Square', 'Yoshinori Kitase', 30);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('Cave Story', 'Platformer', 2004, 'Nicalis', 'Pixel', 10);
+insert into Videogioco(titolo, genere, anno, azienda, produttore, prezzo) values('KrunkerIO', 'FPS', 2015, NULL, NULL, NULL);
+
+insert into VideogiocoMultiplayer values(1, 1, 2);
+insert into VideogiocoMultiplayer values(5, 4, 10);
+insert into VideogiocoMultiplayer values(6, 1, 3);
+insert into VideogiocoMultiplayer values(8, 2, 4);
+insert into VideogiocoMultiplayer values(11, 3, 10);
+insert into VideogiocoMultiplayer values(14, 1, 10);
+
+insert into TipologiaPiano values('Gratuito', 0);
+insert into TipologiaPiano values('Mensile', 10);
+insert into TipologiaPiano values('Annuale', 100);
+
